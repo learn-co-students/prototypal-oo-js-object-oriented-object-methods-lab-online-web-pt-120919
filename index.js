00b8253a6907912b55ea30039c15c691c8edd5ed
Mely07 <damely.tineo07@gmail.com>
2020-08-15 23:23:08 -0400

    Done.

--- index.js
+++ index.js
@@ -2,24 +2,24 @@ function BoardMember(name, homeState, training) {
     this.name = name;
     this.homeState = homeState;
     this.training = training;
+}
 
-    this.prototype.veto = function() {
-        return "No, I must disagree"
-    }
+BoardMember.prototype.veto = function() {
+    return "No, I must disagree"
+}
 
-    this.prototype.approve = function() {
-        return "You can do that!"
-    }
-    
-    this.prototype.doCharity = function() {
-        return "I like to help people."
-    }
+BoardMember.prototype.approve = function() {
+    return "You can do that!"
+}
 
-    this.prototype.releasePressStatement = function() {
-        return "You will see great things from Scuber."
-    }
+BoardMember.prototype.doCharity = function() {
+    return "I like to help people."
+}
 
-    this.prototype.sayHi  = function(){
-        return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
-    }
+BoardMember.prototype.releasePressStatement = function() {
+    return "You will see great things from Scuber."
+}
+
+BoardMember.prototype.sayHi  = function(){
+    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
 }
